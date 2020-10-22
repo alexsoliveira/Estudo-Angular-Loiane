c50384593aceda232a362b47a8c9d699935f66bb
alexsoliveira <alexsoliveira@gmail.com>
2020-10-22 18:59:18 -0300

    Estudo Angular

---
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'contador',
+  templateUrl: './output-property.component.html',
+  styleUrls: ['./output-property.component.css']
+})
+export class OutputPropertyComponent implements OnInit {
+
+  @Input() valor: number = 0;
+
+  @Output() mudouValor = new EventEmitter();
+
+  @ViewChild('campoInput') campoValorInput: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  incrementa(){
+    //this.valor++;    
+    this.campoValorInput.nativeElement.value++;
+    this.mudouValor.emit({novoValor: this.valor});
+  }
+
+  decrementa(){
+    //this.valor--;
+    this.campoValorInput.nativeElement.value--;
+    this.mudouValor.emit({novoValor: this.valor});
+  }
+
+}
